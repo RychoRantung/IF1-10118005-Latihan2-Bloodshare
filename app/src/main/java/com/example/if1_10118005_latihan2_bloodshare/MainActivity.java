f573026e74696141775c1a8c154ed92228bd2dce
RychoRantung <rantungrico@gmail.com>
2021-04-30 16:26:40 +0800

    Latihan2 Bloodshare commit

--- app/src/main/java/com/example/if1_10118005_latihan2_bloodshare/MainActivity.java
+++ app/src/main/java/com/example/if1_10118005_latihan2_bloodshare/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.if1_10118005_latihan2_bloodshare;
 
+/*
+    * NIM: 10118005
+    * Nama: Rycho Rantung
+    * Kelas: IF-1
+    * Tanggal Pengerjaan: 28 April 2021
+    * Deskripsi Pekerjaan: Membuat project aplikasi bloodshare. Aplikasi ini hanya bisa berpindah activity saja
+
+ */
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
